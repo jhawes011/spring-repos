23629a3ceb0ae81c02383e84673053d481585b3e
Joe Hawes <jhawes011@gmail.com>
2025-03-12 15:24:34 -0400

    Update ActorController.java

--- bmdb-java-web/src/main/java/com/bmdb/controller/ActorController.java
+++ bmdb-java-web/src/main/java/com/bmdb/controller/ActorController.java
@@ -5,15 +5,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.bmdb.db.ActorRepo;
@@ -27,25 +20,27 @@ public class ActorController {
 
 	@Autowired
 	private ActorRepo actorRepo;
-	
+
 	@GetMapping("/")
 	public List<Actor> getAll() {
 		return actorRepo.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
-	public Optional<Actor> getById(@PathVariable int id) {
+	public Optional<Actor> getById(@PathVariable int id){
 		Optional<Actor> a = actorRepo.findById(id);
 		if (a.isPresent()) {
-			return a;
+			return a; // Return the movie if found
 		} else {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Actor not found for id " + id);
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Actor not found for id " + id); // Return null if
+																										// not found
 		}
 	}
 	@PostMapping("")
 	public Actor add(@RequestBody Actor actor) {
 		return actorRepo.save(actor);
 	}
+
 	@PutMapping("/{id}")
 	public void putActor(@PathVariable int id, @RequestBody Actor actor) {
 		if (id != actor.getId()) {
@@ -57,6 +52,7 @@ public class ActorController {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Actor not found for id " + id);
 		}
 	}
+
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable int id) {
 		if (actorRepo.existsById(id)) {
