508ec8d860b23e6cdb5e01fc06d17e491187d61c
Joe Hawes <jhawes011@gmail.com>
2025-03-13 11:06:07 -0400

    Created Product Interface and classes

--- bmdb-java-web/src/main/java/com/bmdb/model/Credit.java
+++ bmdb-java-web/src/main/java/com/bmdb/model/Credit.java
@@ -1,41 +1,50 @@
 package com.bmdb.model;
 
 import jakarta.persistence.*;
+
 @Entity
 public class Credit {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private int id;
-@ManyToOne
-@JoinColumn(name = "actorID")
-private Actor actor;
-@ManyToOne
-@JoinColumn(name = "movieID")
-private Movie movie;
-private String role;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public Actor getActor() {
-	return actor;
-}
-public void setActor(Actor actor) {
-	this.actor = actor;
-}
-public Movie getMovie() {
-	return movie;
-}
-public void setMovie(Movie movie) {
-	this.movie = movie;
-}
-public String getRole() {
-	return role;
-}
-public void setRole(String role) {
-	this.role = role;
-}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@ManyToOne
+	@JoinColumn(name = "actorID")
+	private Actor actor;
+	@ManyToOne
+	@JoinColumn(name = "movieID")
+	private Movie movie;
+	private String role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Actor getActor() {
+		return actor;
+	}
+
+	public void setActor(Actor actor) {
+		this.actor = actor;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 }
